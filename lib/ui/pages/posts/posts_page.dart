90dd11d46b8658f147eb84a55fbd1f91f336b193
Huseyn Tahirov <huseyntahirovv@gmail.com>
2025-01-10 20:59:48 +0400

    - posts api integrated with ui

--- lib/ui/pages/posts/posts_page.dart
+++ lib/ui/pages/posts/posts_page.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
 
-class PostsPage extends StatelessWidget {
+import '../../../data/models/remote/post_response.dart';
+import '../../../data/services/post_service.dart';
+
+class PostsPage extends StatefulWidget {
   const PostsPage({super.key});
+
+  @override
+  State<PostsPage> createState() => _PostsPageState();
+}
+
+class _PostsPageState extends State<PostsPage> {
+  late Future<List<PostResponse>> futurePosts;
+
+  @override
+  void initState() {
+    futurePosts = PostService.getPosts();
+    super.initState();
+  }
+
+  void _refreshPosts() {
+    futurePosts = PostService.getPosts();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,26 +30,44 @@ class PostsPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Posts'),
       ),
-      body: ListView.separated(
-        itemCount: 10,
-        padding: const EdgeInsets.all(16),
-        separatorBuilder: (_, __) => const SizedBox(height: 16),
-        itemBuilder: (_, i) {
-          return const ListTile(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(12)),
-              side: BorderSide(color: Colors.grey),
-            ),
-            leading: Text(
-              '1',
-              style: TextStyle(
-                fontSize: 16,
+      body: FutureBuilder(
+          future: futurePosts,
+          builder: (_, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            List<PostResponse> posts = snapshot.data ?? [];
+            return RefreshIndicator(
+              onRefresh: () async => _refreshPosts(),
+              child: ListView.separated(
+                itemCount: posts.length,
+                padding: const EdgeInsets.all(16),
+                separatorBuilder: (_, __) => const SizedBox(height: 16),
+                itemBuilder: (_, i) {
+                  PostResponse post = posts[i];
+                  return ListTile(
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      side: BorderSide(color: Colors.grey),
+                    ),
+                    leading: Text(
+                      '${post.id}',
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    title: Text(post.title),
+                    subtitle: post.body == null ? null : Text(post.body!),
+                  );
+                },
               ),
-            ),
-            title: Text('Title'),
-            subtitle: Text('Subtitle'),
-          );
-        },
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _refreshPosts,
+        child: const Icon(Icons.refresh),
       ),
     );
   }
